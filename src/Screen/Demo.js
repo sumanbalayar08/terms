fe932f764a60f3b5b70a7ad07c8b0bf76ff1ec12
SumanBalayar <orthohydrogen123@gmail.com>
2023-12-18 13:57:29 +0545

    final

--- src/Screen/Demo.js
+++ src/Screen/Demo.js
@@ -1,11 +1,9 @@
-import React, { useEffect,useState } from 'react';
-import image2 from '../images/geiranger.jpg'
-import axios from 'axios';
-import Navbar from '../Components/Navbar';
-
+import React, { useEffect, useState } from "react";
+import image2 from "../images/geiranger.jpg";
+import axios from "axios";
+import Navbar from "../Components/Navbar";
 
 const Demo = () => {
-
   const [text, setText] = useState(null);
 
   const handleClose = () => {
@@ -26,40 +24,43 @@ const Demo = () => {
     };
     fetchdata();
   }, []);
- 
+
   return (
-    <div className='relative '>
-      <img src={image2} alt="background image" className='z-0 h-full w-full object-fill fixed'/>
-<div className='z-10 relative'>
-      <Navbar />
-      <div className="flex-1 py-[4%] space-y-6">
-        <h1 className="flex items-center justify-center text-white font-bold text-2xl">
-          Terms
-        </h1>
-        <div className="flex items-center justify-center font-bold text-lg">
-          <button
-            className="text-white bg-green-700 rounded-full px-8 py-3"
-            onClick={handleClose}
-          >
-            Close and Go Back
-          </button>
-        </div>
-        <div className="flex items-center whitespace-pre-line justify-center text-black bg-white rounded-2xl px-10 mx-5 sm:mx-14 md:mx-28 xl:mx-60 py-8 text-center font-serif">
-          {text ? <p>{text}</p> : <p>Server Not Active</p>}
-        </div>
-        <div className="flex items-center justify-center font-bold text-lg">
-          <button
-            className="text-white bg-green-700 rounded-full px-8 py-3"
-            onClick={handleClose}
-          >
-            Close and Go Back
-          </button>
+    <div className="relative h-screen xl:h-min">
+      <img
+        src={image2}
+        className="absolute z-0 h-full w-full object-fill"
+        alt="h"
+      />
+      <div className="z-10 relative">
+        <Navbar />
+        <div className="flex-1 py-[4%] space-y-6">
+          <h1 className="flex items-center justify-center text-white font-bold text-2xl">
+            Terms
+          </h1>
+          <div className="flex items-center justify-center font-bold text-lg">
+            <button
+              className="text-white bg-green-700 rounded-full px-8 py-3"
+              onClick={handleClose}
+            >
+              Close and Go Back
+            </button>
+          </div>
+          <div className="flex items-center whitespace-pre-line justify-center text-black bg-white rounded-2xl px-10 mx-5 sm:mx-14 md:mx-28 xl:mx-60 py-8 text-center font-serif">
+            {text ? <p>{text}</p> : <p>Server Not Active</p>}
+          </div>
+          <div className="flex items-center justify-center font-bold text-lg">
+            <button
+              className="text-white bg-green-700 rounded-full px-8 py-3"
+              onClick={handleClose}
+            >
+              Close and Go Back
+            </button>
+          </div>
         </div>
       </div>
     </div>
-    </div>
-
-);
+  );
 };
 
 export default Demo;
--- src/Screen/Terms.js
+++ src/Screen/Terms.js
@@ -27,7 +27,7 @@ const Terms = () => {
   return (
     <div className="bg-[url('./images/geiranger.jpg')] overflow-auto bg-cover bg-fixed h-screen">
       <Navbar />
-      <div className="flex-1 py-[4%] space-y-6">
+      <div className="flex-1 py-[4%] space-y-6 scr">
         <h1 className="flex items-center justify-center text-white font-bold text-2xl">
           Terms
         </h1>
